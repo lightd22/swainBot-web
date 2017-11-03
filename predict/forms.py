4a3619bb296f6263eecaf6a4dea18b2d53ee1f53
Devin Light <naavi09@gmail.com>
2017-11-03 09:49:56 -0700

    Finally got loaddata to work. Retrying with form choices populated

--- predict/forms.py
+++ predict/forms.py
@@ -4,8 +4,8 @@ from .models import Champion, Position
 class DraftForm(forms.Form):
     CHAMPIONS = Champion.objects.order_by('display_name')
     CHAMPION_CHOICES = []
-#    for champion in CHAMPIONS:
-#        CHAMPION_CHOICES.append((champion.id, champion.display_name))
+    for champion in CHAMPIONS:
+        CHAMPION_CHOICES.append((champion.id, champion.display_name))
 
     PICK_CHOICES = [("","")]+CHAMPION_CHOICES
     NO_BAN = -1
@@ -13,8 +13,8 @@ class DraftForm(forms.Form):
 
     POSITIONS = Position.objects.order_by('id')
     POSITION_CHOICES = [(None,"")]
-#    for position in POSITIONS:
-#        POSITION_CHOICES.append((position.id, position.display_name))
+    for position in POSITIONS:
+        POSITION_CHOICES.append((position.id, position.display_name))
 
     blue_style_class = "blue"
     red_style_class = "red"
