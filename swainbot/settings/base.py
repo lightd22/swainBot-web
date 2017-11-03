1935d5f7eae0d1641589a7e62fa4508096fdcb62
Devin Light <naavi09@gmail.com>
2017-11-02 18:11:12 -0700

    Still working on heroku bugs.

--- swainbot/settings/base.py
+++ swainbot/settings/base.py
@@ -121,8 +121,3 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
