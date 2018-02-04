04728d848b31f90a5f27091561877597d9488134
Devin Light <naavi09@gmail.com>
2018-02-03 17:20:41 -0800

    fixed no_ban missing error

--- predict/forms.py
+++ predict/forms.py
@@ -2,18 +2,18 @@ from django import forms
 from .models import Champion, Position
 
 class DraftForm(forms.Form):
+    NO_BAN = -1
+    POSITION_CHOICES = [(None,""),
+                        (1,"ADC"),
+                        (2,"MID"),
+                        (3,"TOP"),
+                        (4,"JNG"),
+                        (5,"SUP")]
     def __init__(self, *args, **kwargs):
         super(DraftForm, self).__init__(*args, **kwargs)
         teams = ["blue", "red"]
         NUM_PICKS = 5
         NUM_BANS = 5
-
-        self.POSITION_CHOICES = [(None,""),
-                            (1,"ADC"),
-                            (2,"MID"),
-                            (3,"TOP"),
-                            (4,"JNG"),
-                            (5,"SUP")]
 
         CHAMPIONS = Champion.objects.order_by('display_name')
         CHAMPION_CHOICES = []
@@ -21,8 +21,7 @@ class DraftForm(forms.Form):
             CHAMPION_CHOICES.append((champion.id, champion.display_name))
 
         PICK_CHOICES = [("","")]+CHAMPION_CHOICES
-        NO_BAN = -1
-        BAN_CHOICES = [("",""),(NO_BAN,"No Ban")]+CHAMPION_CHOICES
+        BAN_CHOICES = [("",""),(DraftForm.NO_BAN,"No Ban")]+CHAMPION_CHOICES
 
         blue_style_class = "blue"
         red_style_class = "red"
@@ -39,4 +38,4 @@ class DraftForm(forms.Form):
                 self.fields[name] = forms.ChoiceField(widget=forms.Select(attrs=widget_attrs), label="Pick {}".format(pick_count), initial="", choices=PICK_CHOICES, required=False)
 
                 name = "{}_{}_{}".format(team, "pos", pick_count)
-                self.fields[name] = forms.TypedChoiceField(coerce=int, empty_value=None, widget=forms.Select(attrs=widget_attrs), label="Pos", choices=self.POSITION_CHOICES, required=False)
+                self.fields[name] = forms.TypedChoiceField(coerce=int, empty_value=None, widget=forms.Select(attrs=widget_attrs), label="Pos", choices=DraftForm.POSITION_CHOICES, required=False)
